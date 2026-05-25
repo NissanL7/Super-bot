6cf91a0d568cd0c8acc1a9dca879561169f733ed
NissanL7 <n38482688@gmail.com>
2026-05-26 02:09:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"✅ Bot is online as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
