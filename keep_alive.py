b3e9382b08c731a42593f8a609b1957d617128a2
NissanL7 <n38482688@gmail.com>
2026-06-02 10:10:38 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,6 +2,6 @@
 - async def handle(request):
 - return web.Response(text="Bot is alive!")
 - def keep_alive():
-    app = web.Application()
-    app.add_routes([web.get('/', handle)])
-    web.run_app(app, host="0.0.0.0", port=8080)
+- app = web.Application()
+- app.add_routes([web.get('/', handle)])
+- web.run_app(app, host="0.0.0.0", port=8080)
